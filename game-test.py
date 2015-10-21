047b0e7be86b1a114b2a36eaf3394eee467d8523
emmadevere <emmadevere@gmail.com>
2015-10-21 17:26:59 -0400

    Created movable icon

--- game-test.py
+++ game-test.py
@@ -9,7 +9,7 @@
 """
  
 import pygame
- 
+import random
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -21,7 +21,10 @@ SBROWN = (244,164,96)
 BWOOD = (222,184,135)
 CHIFFON = (255,250,205)
 SBLUE = (176,196,222)
-ORANGE = (204,102,0)
+ORANGE = (254,196,79)
+PERU = (205,133,63)
+DORANGE = (217,95,14)
+LORANGE = (255,247,188)
 pygame.init()
  
 # Set the width and height of the screen [width, height]
@@ -30,11 +33,24 @@ screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("My Game")
  
+def draw_icon(screen, x, y):
+    pygame.draw.ellipse(screen, BLACK, [1+x,y,10,10], 0) 
+ 
+# Speed in pixels per frame
+x_speed = 0
+y_speed = 0
+ 
+# Current position
+x_coord = 300
+y_coord = 300
 # Loop until the user clicks the close button.
 done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
+
+# Hide the mouse cursor
+pygame.mouse.set_visible(False)
  
 # -------- Main Program Loop -----------
 while not done:
@@ -42,9 +58,34 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
- 
+            
+            # User pressed down on a key
+        elif event.type == pygame.KEYDOWN:
+            # Figure out if it was an arrow key. If so
+            # adjust speed.
+            if event.key == pygame.K_LEFT:
+                x_speed = -3
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 3
+            elif event.key == pygame.K_UP:
+                y_speed = -3
+            elif event.key == pygame.K_DOWN:
+                y_speed = 3
+             
+                # User let up on a key
+        elif event.type == pygame.KEYUP:
+            # If it is an arrow key, reset vector back to zero
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_speed = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_speed = 0 
     # --- Game logic should go here   
- 
+
+    # Move the object according to the speed vector.
+    x_coord += x_speed
+    y_coord += y_speed
+    
+  
     # --- Drawing code should go here
  
     # First, clear the screen to white. Don't put other drawing commands
@@ -56,14 +97,17 @@ while not done:
     pygame.draw.rect(screen,ORANGE,[20,40,50,400],0)
     pygame.draw.rect(screen,ORANGE,[625,40,50,400],0)
     pygame.draw.rect(screen,ORANGE,[20,400,655,50],0)
-    pygame.draw.rect(screen,CHIFFON,[70,120,560,280],0)
+    pygame.draw.rect(screen,LORANGE,[70,120,560,280],0)
     
     # Draw on the screen several lines from (0,10) to (100,110)
     # 5 pixels wide using a for loop
     for y_offset in range(0, 100, 10):
-        pygame.draw.line(screen,BLACK,[674,20+y_offset],[20,20+y_offset],2)   
+        pygame.draw.line(screen,DORANGE,[674,20+y_offset],[20,20+y_offset],2)  
+    # Icon
+    draw_icon(screen, x_coord, y_coord)        
  
     # --- Go ahead and update the screen with what we've drawn.
+
     pygame.display.flip()
  
     # --- Limit to 60 frames per second
